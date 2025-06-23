b2e4cf94b76fe881a2634cf86abf9a34a8ac15ca
Prathika M <prathimuthu04@gmail.com>
2025-06-23 14:58:28 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Prathika M",
+    author_email="prathimuthu04@gmail.com",
+    packages=find_packages()
+)
